c6cd9355ed6f28158e209a8b8232a76f40c31a5b
melissa-murphy <melissamurphy.ca@gmail.com>
2020-02-24 09:50:49 -0800

    build

---
+++ build/precache-manifest.f39be9a15280f93bc99ef546c4884b54.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e1fdfb121c6bdd1adab660bce7d060d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14b7116777e348af7750",
+    "url": "/static/js/2.ffc354e6.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.ffc354e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8be739fee31e125b1f2f",
+    "url": "/static/js/main.8d9ba81d.chunk.js"
+  },
+  {
+    "revision": "843b3f90f18cd4ab02a1",
+    "url": "/static/js/runtime-main.742b6627.js"
+  },
+  {
+    "revision": "4dd010412897424380349308f8c53f10",
+    "url": "/static/media/gli_home.4dd01041.png"
+  },
+  {
+    "revision": "54768117fb1a57a8698478193c6f982e",
+    "url": "/static/media/goldengate.54768117.jpg"
+  },
+  {
+    "revision": "bd5012039ac684b9b490f51b7d37204f",
+    "url": "/static/media/melissa-murphy-img.bd501203.jpg"
+  },
+  {
+    "revision": "696b7e9ba8f5e6c42c381ef0a3a0be38",
+    "url": "/static/media/sps_home.696b7e9b.png"
+  }
+]);
